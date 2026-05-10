44af535b3ca8058029d1a9769af09e1a2935700b
Ameen shariff <ameenshariff0319@gmail.com>
2026-05-11 04:27:11 +0530

    production deploy

--- src/controllers/user.ctrl.js
+++ src/controllers/user.ctrl.js
@@ -16,8 +16,8 @@ const E164_REGEX         = /^\+[1-9]\d{7,14}$/
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure:   process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  secure:   true,
+  sameSite: 'none',
   maxAge:   7 * 24 * 60 * 60 * 1000,
 }
 
